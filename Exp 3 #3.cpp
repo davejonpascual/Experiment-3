439c618bb7b777a110e01e79fe95073ca3306b72
David Pascual <47467313+davejonpascual@users.noreply.github.com>
2019-05-23 04:25:01 +0800

    Update Exp 3 #3.cpp

--- Exp 3 #3.cpp
+++ Exp 3 #3.cpp
@@ -5,15 +5,15 @@ using namespace std;
   {
    int a[20],b[20],i,j,size;
    cout<<"How many elements you want to enter: ";
-   cin>>n;
-   cout<<"Enter any "<<n<<" elements in Array: ";
-   for(i=0; i<n ;i++)
+   cin>>size;
+   cout<<"Enter any "<<size<<" elements in Array: ";
+   for(i=0; i<size ;i++)
    {
    cin>>a[i];
    }
    cout<<"Reverse of Array: ";
 
-   for(i=n-1,j=0; i>=0;i--,j++)
+   for(i=size-1,j=0; i>=0;i--,j++)
    {
     b[i]=a[j];
    }
